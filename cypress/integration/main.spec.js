2ed4510c78921987af48679bda31cf1c69dba673
vnalupta <vnalupta@grubhub.com>
2020-08-10 09:47:58 -0400

    not sure whats wrong; run tests again

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -21,17 +21,17 @@ describe('Navigation', () => {
         cy.visit('/');
         cy.get('nav').click();
         cy.wait(1500);
-        cy.get('nav li').contains('Home').should('be.visible');
-        cy.get('nav li').contains('Work').should('be.visible');
-        cy.get('nav li').contains('Blog').should('be.visible');
-        cy.get('nav li').contains('CV').should('be.visible');
+        cy.get('nav ul li').contains('Home').should('be.visible');
+        cy.get('nav ul li').contains('Work').should('be.visible');
+        cy.get('nav ul li').contains('Blog').should('be.visible');
+        cy.get('nav ul li').contains('CV').should('be.visible');
 
         cy.get('nav').click();
         cy.wait(1500);
-        cy.get('nav li').contains('Home').should('not.be.visible');
-        cy.get('nav li').contains('Work').should('not.be.visible');
-        cy.get('nav li').contains('Blog').should('not.be.visible');
-        cy.get('nav li').contains('CV').should('not.be.visible');
+        cy.get('nav ul li').contains('Home').should('not.be.visible');
+        cy.get('nav ul li').contains('Work').should('not.be.visible');
+        cy.get('nav ul li').contains('Blog').should('not.be.visible');
+        cy.get('nav ul li').contains('CV').should('not.be.visible');
     })
 
     it('should navigate to cv from home', () => {
