67aa61b6252e6e572d158b11bd2e983042e5c876
Vincent Nalupta <vnalupta@gmail.com>
2020-08-10 06:21:37 -0400

    Remove vince from SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const IndexPage = props => {
                     `engineering manager`,                    
                     `snailbites`,
             ]}
-            description="Vincent (Vince) Nalupta's site"
+            description="Vincent Nalupta's site"
             />
             <StyledHeader>
                 <Jumbotron />
